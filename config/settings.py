b0ebe458fe2eb1282052b766c88b696d13d1333f
Hammad Ali <hammadali963@gmail.com>
2022-08-16 23:29:09 +0500

    fixes static files

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/django-static/'
-STATIC_ROOT = BASE_DIR / 'static-files'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
